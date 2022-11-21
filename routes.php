e5eea83718c98ad39d1a154651cf2a482c17695c
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-11-21 14:37:57 +0100

    Added SurrealDB driver

--- routes.php
+++ routes.php
@@ -7,9 +7,9 @@ get('/php-exam', 'views/index');
 get('/php-exam/home', 'views/home');
 get('/php-exam/user/$id', 'views/user');
 
-any('/php-exam/404', 'views/404');
-
 // API
-post('/php-exam/api-v1/user/$id', 'api/create_user');
-put('/php-exam/api-v1/user/$id', 'api/update_user');
-delete('/php-exam/api-v1/user/$id', 'api/delete_user');
+post('/php-exam/user/$id', 'api/create_user');
+put('/php-exam/user/$id', 'api/update_user');
+delete('/php-exam/user/$id', 'api/delete_user');
+
+any('/php-exam/404', 'views/404');
---
+++ surrealdb.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+Start SurrealDB server in the terminal
+surreal start --log debug --user root --pass root memory
+Start SurrealDB server - save data in a folder called "data"
+surreal start --log debug --user root --pass root file:data
+SurrealDB client
+surreal sql --conn http://localhost:8000 --user root --pass root --ns company --db company --pretty
+*/
+
+function surrealdb($query, $lets = [], $host = 'http://127.0.0.1', $port = 8000)
+{
+  try {
+    // Create secure lets
+    $variables = '';
+    foreach ($lets as $key => $value) {
+      $variables .= 'LET $' . $key . '="' . $value . '";';
+    }
+    // echo $variables;
+    // replace variables with place holders
+    $query = str_replace(':', '$', $query);
+    $query = $variables . $query;
+
+    $headers = [
+      'Accept: application/json',
+      'ns: company',
+      'db: company'
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://127.0.0.1:8000/sql");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Do not output the response automatically on exec
+    curl_setopt($ch, CURLOPT_TIMEOUT, 1); //timeout in seconds
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, 'root:root');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $response = curl_exec($ch);
+    if (!$response) {
+      throw new Exception('Cannot connect to SurrealDB');
+    }
+    curl_close($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE) ?? "500"; // MAC maybe comment this out
+    if ($http_code != 200) {
+      throw new Exception($response);
+    } // MAC maybe comment this out
+
+    return $response;
+    exit();
+  } catch (Exception $ex) {
+    throw new Exception($ex->getMessage());
+  }
+}
