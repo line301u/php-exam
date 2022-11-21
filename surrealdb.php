11dbecac236b4b74de170a664c54746f35af115d
linealind <linea.lindebjerg@hotmail.com>
2022-11-21 15:37:18 +0100

    auth conn fix

--- surrealdb.php
+++ surrealdb.php
@@ -4,9 +4,9 @@
 Start SurrealDB server in the terminal
 surreal start --log debug --user root --pass root memory
 Start SurrealDB server - save data in a folder called "data"
-surreal start --log debug --user root --pass root file:data
+surreal start --log debug --user admin --pass password file:database.db
 SurrealDB client
-surreal sql --conn http://localhost:8000 --user root --pass root --ns company --db company --pretty
+surreal sql --conn http://localhost:8000 --user admin --pass password --ns php-exam --db php-exam --pretty
 */
 
 function surrealdb($query, $lets = [], $host = 'http://127.0.0.1', $port = 8000)
@@ -33,17 +33,17 @@ function surrealdb($query, $lets = [], $host = 'http://127.0.0.1', $port = 8000)
     curl_setopt($ch, CURLOPT_TIMEOUT, 1); //timeout in seconds
     curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($ch, CURLOPT_USERPWD, 'root:root');
+    curl_setopt($ch, CURLOPT_USERPWD, 'admin:password');
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     $response = curl_exec($ch);
     if (!$response) {
       throw new Exception('Cannot connect to SurrealDB');
     }
     curl_close($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE) ?? "500"; // MAC maybe comment this out
-    if ($http_code != 200) {
-      throw new Exception($response);
-    } // MAC maybe comment this out
+    // $http_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE) ?? "500"; // MAC maybe comment this out
+    // if ($http_code != 200) {
+    //   throw new Exception($response);
+    // } // MAC maybe comment this out
 
     return $response;
     exit();
--- views/home.php
+++ views/home.php
@@ -1,15 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+$title = "Home";
+require_once __DIR__ . '/header.php'; 
+?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-
-<body>
+<section>
   <h1>Home</h1>
-</body>
+  <h2>All users</h2>
 
-</html>
+  <?php
+  require_once __DIR__.'/../surrealdb.php';
+  ini_set('display_errors', 1);
+
+  try{
+    $users = surrealdb('SELECT * FROM user');
+    echo $users;
+
+  } catch (Exception $ex){
+      echo $ex;
+  }
+  ?>
+</section>
+
+<?php require_once __DIR__ . '/footer.php'; ?>
