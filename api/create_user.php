dac158bd717e0185066f1d4dcdcb8ff4ea16ecb0
Signe <sign4731@stud.kea.dk>
2022-11-21 16:05:15 +0100

    sign up and almost log in

--- api/create_user.php
+++ api/create_user.php
@@ -1,3 +1,27 @@
 <?php
+require_once __DIR__ . '/../surrealdb.php';
 
-echo "hi";
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+try {
+    if (!isset($first_name)) {
+        header("Location: /php-exam");
+    }
+    if (!isset($last_name)) {
+        header("Location: /php-exam");
+    }
+    if (!isset($email)) {
+        header("Location: /php-exam");
+    }
+    if (!isset($password)) {
+        header("Location: /php-exam");
+    }
+
+    surrealdb('CREATE user SET first_name=:first_name, last_name=:last_name, email=:email, password=:password', ['first_name' => $first_name, 'last_name' => $last_name, 'email' => $email, 'password' => $password]);
+    header("Location: /php-exam");
+} catch (Exception $ex) {
+    echo $ex;
+}
--- routes.php
+++ routes.php
@@ -6,9 +6,11 @@ require_once __DIR__ . '/router.php';
 get('/php-exam', 'views/index');
 get('/php-exam/home', 'views/home');
 get('/php-exam/user/$id', 'views/user');
+get('/php-exam/login', 'views/user');
 
 // API
-post('/php-exam/user/$id', 'api/create_user');
+post('/php-exam/log_in', 'api/log_in');
+post('/php-exam/create-user', 'api/create_user');
 put('/php-exam/user/$id', 'api/update_user');
 delete('/php-exam/user/$id', 'api/delete_user');
 
