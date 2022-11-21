941ead4da2b52c08d8b15684e97627052dc7e5e0
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-11-21 13:33:28 +0100

    New user

---
+++ classes/User.php
@@ -0,0 +1,38 @@
+<?php
+
+class Model
+{
+  public function getOne()
+  {
+  }
+
+  public function getMany()
+  {
+  }
+}
+
+// class User extends Model {
+
+// }
+
+// class Property extends Model {
+
+// }
+
+class User
+{
+  public function login()
+  {
+  }
+
+  public function logout()
+  {
+  }
+
+  public function create()
+  {
+  }
+}
+
+$user = new User();
+$user->login();
