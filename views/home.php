8607e86ab212bde3c0d77be1af4523fbe86062dc
linealind <linea.lindebjerg@hotmail.com>
2022-11-23 10:44:32 +0100

    home page

--- views/home.php
+++ views/home.php
@@ -1,6 +1,7 @@
 <?php 
+ini_set('display_errors', 1);
 $title = "Home";
-require_once __DIR__ . '/header.php'; 
+require_once __DIR__ . '/header.php';
 ?>
 
 <section>
@@ -9,14 +10,31 @@ require_once __DIR__ . '/header.php';
 
   <?php
   require_once __DIR__.'/../surrealdb.php';
-  ini_set('display_errors', 1);
 
-  try{
-    $users = surrealdb('SELECT * FROM user');
-    echo $users;
+  $isAdmin = "true";
 
+  try {
+    $users = json_decode(surrealdb('SELECT * FROM user'), true)[0]['result'];
+
+    foreach($users as $user){ ?>
+      <div class="d-flex">
+        <?php if (isset($user['image'])): ?>
+          <img src="<?= out($user['image']) ?>" alt="User profile picture">
+        <?php else: ?>
+          <img src="./images/fallback-profile-pic.png" alt="User profile picture">
+        <?php endif ?>
+        <div>
+          <h4><?= $user['first_name'] ?></h4>
+          <h4><?= $user['last_name'] ?></h4>
+          <p><?= $user['email'] ?></p>
+        </div>
+        <?php if ($isAdmin): ?>
+          <a href="/php-exam/user/<?php echo $user['id'] ?>">Delete user</a>
+        <?php endif ?>
+      </div>
+  <?php }
   } catch (Exception $ex){
-      echo $ex;
+    out($ex);
   }
   ?>
 </section>
