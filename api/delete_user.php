261b0cc2fd2d67bff392a96c5e29fa307574219e
Signe <sign4731@stud.kea.dk>
2022-11-25 13:36:23 +0100

    check if admin on delete user

--- api/delete_user.php
+++ api/delete_user.php
@@ -1,21 +1,25 @@
 <?php
-require_once __DIR__.'/../surrealdb.php';
+require_once __DIR__ . '/../surrealdb.php';
 
 $id = $_POST["id"];
-$isAdmin = true; // FIX !
+$isAdmin = "";
 echo $id;
+// Check if user is admin
+if (isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == 1) {
+    $isAdmin = "true";
+}
 
 if (!isset($id)) {
     header("Location: /404.php");
     exit();
 }
 
-if ($isAdmin){
+if ($isAdmin) {
     try {
-        surrealdb("DELETE :id", ['id'=>$id]);
+        surrealdb("DELETE :id", ['id' => $id]);
         header("Location: /home");
         exit();
-    } catch(Exception $ex){
+    } catch (Exception $ex) {
         echo $ex;
     }
 }
