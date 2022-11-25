2e68ad5248bdc233816cc79014514f8e79afe9d1
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-11-25 11:41:57 +0100

    Refactor validate functions

--- global_validation.php
+++ global_validation.php
@@ -1,7 +1,7 @@
 <?php
 
-define('_USER_NAME_MIN_LEN', 2);
-define('_USER_NAME_MAX_LEN', 20);
+define('_USER_FIRST_NAME_MIN_LEN', 2);
+define('_USER_FIRST_NAME_MAX_LEN', 20);
 define('_USER_LAST_NAME_MIN_LEN', 2);
 define('_USER_LAST_NAME_MAX_LEN', 20);
 define('_USER_PASSWORD_MIN_LEN', 6);
@@ -9,83 +9,82 @@ define('_USER_PASSWORD_MAX_LEN', 50);
 define('_REGEX_EMAIL', '^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$');
 
 // ##############################
-function _validate_user_name()
+function _validate_first_name(string $input_name)
 {
-    $error_message = 'user_name ' . _USER_NAME_MIN_LEN . ' to ' . _USER_NAME_MAX_LEN . ' characters';
-    if (!isset($_POST['user_name'])) {
+    $error_message = $input_name . ' ' . _USER_FIRST_NAME_MIN_LEN . ' to ' . _USER_FIRST_NAME_MAX_LEN . ' characters';
+    if (!isset($_POST[$input_name])) {
         _respond($error_message, 400);
     }
-    $_POST['user_name'] = trim($_POST['user_name']);
-    if (strlen($_POST['user_name']) < _USER_NAME_MIN_LEN) {
+    $_POST[$input_name] = trim($_POST[$input_name]);
+    if (strlen($_POST[$input_name]) < _USER_FIRST_NAME_MIN_LEN) {
         _respond($error_message);
     }
-    if (strlen($_POST['user_name']) > _USER_NAME_MAX_LEN) {
+    if (strlen($_POST[$input_name]) > _USER_FIRST_NAME_MAX_LEN) {
         _respond($error_message);
     }
-    return $_POST['user_name'];
+    return $_POST[$input_name];
 }
 
 // ##############################
-function _validate_user_last_name()
+function _validate_last_name($input_name)
 {
-    $error_message = 'user_last_name' . _USER_LAST_NAME_MIN_LEN . ' to ' . _USER_LAST_NAME_MAX_LEN . ' characters';
-    if (!isset($_POST['user_last_name'])) {
+    $error_message = $input_name . ' ' . _USER_LAST_NAME_MIN_LEN . ' to ' . _USER_LAST_NAME_MAX_LEN . ' characters';
+    if (!isset($_POST[$input_name])) {
         _respond($error_message, 400);
     }
-    $_POST['user_last_name'] = trim($_POST['user_last_name']);
-    if (strlen($_POST['user_last_name']) < _USER_LAST_NAME_MIN_LEN) {
+    $_POST[$input_name] = trim($_POST[$input_name]);
+    if (strlen($_POST[$input_name]) < _USER_LAST_NAME_MIN_LEN) {
         _respond($error_message);
     }
-    if (strlen($_POST['user_last_name']) > _USER_LAST_NAME_MAX_LEN) {
+    if (strlen($_POST[$input_name]) > _USER_LAST_NAME_MAX_LEN) {
         _respond($error_message);
     }
-    return $_POST['user_last_name'];
+    return $_POST[$input_name];
 }
 
 // ##############################
-function _validate_user_password($password)
+function _validate_password($input_name)
 {
-    $error_message = $password . ' ' . _USER_PASSWORD_MIN_LEN . ' to ' . _USER_PASSWORD_MAX_LEN . ' characters';
-    if (!isset($_POST[$password])) {
+    $error_message = $input_name . ' ' . _USER_PASSWORD_MIN_LEN . ' to ' . _USER_PASSWORD_MAX_LEN . ' characters';
+    if (!isset($_POST[$input_name])) {
         _respond($error_message, 400);
     }
-    $_POST[$password] = trim($_POST[$password]);
-    if (strlen($_POST[$password]) < _USER_PASSWORD_MIN_LEN) {
+    $_POST[$input_name] = trim($_POST[$input_name]);
+    if (strlen($_POST[$input_name]) < _USER_PASSWORD_MIN_LEN) {
         _respond($error_message);
     }
-    if (strlen($_POST[$password]) > _USER_PASSWORD_MAX_LEN) {
+    if (strlen($_POST[$input_name]) > _USER_PASSWORD_MAX_LEN) {
         _respond($error_message);
     }
-    return $_POST[$password];
+    return $_POST[$input_name];
 }
 
 // ##############################
-function _validate_user_email()
+function _validate_email($input_name)
 {
-    $error_message = 'user_email invalid';
-    if (!isset($_POST['user_email'])) {
+    $error_message = $input_name . ' ' .  'invalid';
+    if (!isset($_POST[$input_name])) {
         _respond($error_message, 400);
     }
-    $_POST['user_email'] = trim($_POST['user_email']);
-    if (!preg_match('/' . _REGEX_EMAIL . '/', $_POST['user_email'])) {
+    $_POST[$input_name] = trim($_POST[$input_name]);
+    if (!preg_match('/' . _REGEX_EMAIL . '/', $_POST[$input_name])) {
         _respond($error_message, 400);
     }
-    return $_POST['user_email'];
+    return $_POST[$input_name];
 }
 
 // ##############################
-// test.png test.jpg
 
-function _validate_image()
+function _validate_image($input_name)
 {
-    if ($_FILES['item_image']['error'] === UPLOAD_ERR_INI_SIZE) {
+    if ($_FILES[$input_name]['error'] === UPLOAD_ERR_INI_SIZE) {
         _respond('item_image too large', 400);
     }
-    $item_image_temp_name = $_FILES["item_image"]["tmp_name"]; // C:\xampp\tmp\php791.tmp || C:\xampp\tmp\php5245.tmp
+    $item_image_temp_name = $_FILES[$input_name]["tmp_name"]; // C:\xampp\tmp\php791.tmp || C:\xampp\tmp\php5245.tmp
     $target_dir = "images/";
-    $target_file = $target_dir . basename($_FILES["item_image"]["name"]);
+    $target_file = $target_dir . basename($_FILES[$input_name]["name"]);
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION)); // just reads the extension of the file
-    $image_mime = mime_content_type($_FILES["item_image"]["tmp_name"]); // reads the mime inside the file
+    $image_mime = mime_content_type($_FILES[$input_name]["tmp_name"]); // reads the mime inside the file
     $accepted_image_formats = ['image/png', 'image/jpeg'];
     if (!in_array($image_mime, $accepted_image_formats)) {
         http_response_code(400);
@@ -102,7 +101,7 @@ function _validate_image()
             break;
     }
 
-    if (move_uploaded_file($_FILES["item_image"]["tmp_name"], 'images/2.png')) {
+    if (move_uploaded_file($_FILES[$input_name]["tmp_name"], 'images/2.png')) {
         echo 'ok';
         exit();
     }
